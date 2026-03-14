445841edcd1284441b96cc8dbb93e0c496697663
GreenKookie56 <brandon05121@gmail.com>
2026-03-14 13:11:20 +0800

    Add files via upload

---
+++ jokers/nahuatljoker.lua
@@ -0,0 +1,35 @@
+
+SMODS.Joker{ --Nahuatl Joker
+    key = "nahuatljoker",
+    config = {
+        extra = {
+        }
+    },
+    loc_txt = {
+        ['name'] = 'Nahuatl Joker',
+        ['text'] = {
+            [1] = '{C:attention}-1{} rank of each {C:attention}scoring{} card',
+            [2] = 'if played hand is exactly {C:attention}3{} cards'
+        },
+        ['unlock'] = {
+            [1] = 'Unlocked by default.'
+        }
+    },
+    pos = {
+        x = 0,
+        y = 0
+    },
+    display_size = {
+        w = 71 * 1, 
+        h = 95 * 1
+    },
+    cost = 6,
+    rarity = 2,
+    blueprint_compat = false,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'Joker',
+    pools = { ["modprefix_sholiumx_jokers"] = true }
+}
