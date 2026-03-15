9f5e0c5a62a35143953731e05e6c8ea5acdd4955
GreenKookie56 <brandon05121@gmail.com>
2026-03-15 10:45:20 +0800

    remove talisman dependency

--- consumables/sholium.lua
+++ consumables/sholium.lua
@@ -1,5 +1,3 @@
-if Talisman then
-
 SMODS.Consumable {
     key = 'sholium',
     set = 'Spectral',
@@ -75,69 +73,3 @@ SMODS.Consumable {
         return true
     end
 }
-
-else
-
-SMODS.Consumable {
-    key = 'sholium',
-    set = 'Spectral',
-    pos = { x = 2, y = 0 },
-    config = { extra = {
-        hands_value = 1,
-        joker_slots_value = 1
-    } },
-    loc_txt = {
-      ['default'] = {
-        name = 'Sholium',
-        text = {
-        [1] = 'Apply {C:dark_edition}Black Deck{}',
-        [2] = 'to your run',
-        [3] = 'and spawn a {C:attention}Joker{} with',
-        [4] = '{C:dark_edition,E:1}Peculiar{} rarity'
-            }
-        },
-      ['zh_TW'] = {
-		name = "Shol理元素",
-        text = {
-			"賦予{C:dark_edition}黑色牌堆{}效果"
-        },
-      },
-    },
-    cost = 4,
-    unlocked = true,
-    discovered = true,
-    hidden = false,
-    can_repeat_soul = false,
-    atlas = 'CustomConsumables',
-    use = function(self, card, area, copier)
-        local used_card = copier or card
-            G.E_MANAGER:add_event(Event({
-                trigger = 'after',
-                delay = 0.4,
-                func = function()
-                    card_eval_status_text(used_card, 'extra', nil, nil, nil, {message = "-"..tostring(1).." Hand", colour = G.C.RED})
-                    
-        G.GAME.round_resets.hands = G.GAME.round_resets.hands - 1
-        ease_hands_played(-1)
-        
-                    return true
-                end
-            }))
-            delay(0.6)
-            G.E_MANAGER:add_event(Event({
-                trigger = 'after',
-                delay = 0.4,
-                func = function()
-                    card_eval_status_text(used_card, 'extra', nil, nil, nil, {message = "+"..tostring(1).." Joker Slot", colour = G.C.DARK_EDITION})
-                    G.jokers.config.card_limit = G.jokers.config.card_limit + 1
-                    return true
-                end
-            }))
-            delay(0.6)
-    end,
-    can_use = function(self, card)
-        return true
-    end
-}
-
-end
