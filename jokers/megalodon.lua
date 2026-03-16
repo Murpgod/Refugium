4ab0d8201fd88299fc96cd8a8f5507fa179060fb
GreenKookie56 <brandon05121@gmail.com>
2026-03-16 19:49:39 +0800

    rework

--- jokers/megalodon.lua
+++ jokers/megalodon.lua
@@ -1,20 +1,21 @@
-if not next(SMODS.find_mod("Cryptid")) then
-  return nil
-end
 
 SMODS.Joker{ --Megalodon
     key = "megalodon",
     config = {
         extra = {
-            mult = 700
+            mult = 1,
+            chips = 1,
+            scale = 1
         }
     },
     loc_txt = {
         ['name'] = 'Megalodon',
         ['text'] = {
-            [1] = 'All played cards give {C:red}+#1#{} Mult when {C:attention}scored{}',
-            [2] = 'and are {C:red}destroyed{} afterwards',
-            [3] = '{C:inactive}Art by 1.2m^2 Fungus Room{}'
+            [1] = 'When a {C:red}Mult Card{}/{C:blue}Bonus Card{}',
+            [2] = 'is scored, this Joker gains',
+            [3] = '{X:red,C:white}X#3#{} Mult/{X:blue,C:white}X#3#{} Chips respectively',
+            [4] = '{C:inactive}(Currently{} {X:red,C:white}X#1#{}{C:inactive} Mult, {}{X:blue,C:white}X#2#{}{C:inactive} Chips){}',
+            [5] = '{C:inactive}Art by 1.2m^2 Fungus Room{}'
         },
         ['unlock'] = {
             [1] = ''
@@ -38,37 +39,44 @@ SMODS.Joker{ --Megalodon
     discovered = true,
     atlas = 'CustomJokers',
     in_pool = function(self, args)
-          return (
-          not args 
-          or args.source ~= 'sho' 
-          or args.source == 'buf' or args.source == 'jud' or args.source == 'rif' or args.source == 'rta' or args.source == 'sou' or args.source == 'uta' or args.source == 'wra'
-          )
-          and true
-      end,
-
-    loc_vars = function(self, info_queue, card)
-        return {vars = {card.ability.extra.mult}}
+        return (
+            not args 
+            or args.source ~= 'sho' 
+            or args.source == 'buf' or args.source == 'jud' or args.source == 'rif' or args.source == 'rta' or args.source == 'sou' or args.source == 'uta' or args.source == 'wra'
+        )
+        and true
     end,
-
+    
+    loc_vars = function(self, info_queue, card)
+        
+        return {vars = {card.ability.extra.mult, card.ability.extra.chips, card.ability.extra.scale}}
+    end,
+    
     calculate = function(self, card, context)
-        if context.destroy_card and context.destroy_card.should_destroy  then
-            return { remove = true }
-        end
-        if context.individual and context.cardarea == G.play  then
-            context.other_card.should_destroy = false
-                context.other_card.should_destroy = true
+        if context.individual and context.cardarea == G.play  and not context.blueprint then
+            if SMODS.get_enhancements(context.other_card)["m_mult"] == true then
+                card.ability.extra.mult = (card.ability.extra.mult) + card.ability.extra.scale
                 return {
-                    mult = card.ability.extra.mult,
-                    extra = {
-                        message = "Thrash!",
-                        colour = G.C.RED
-                        }
+                    message = "Upgrade!"
                 }
+            elseif SMODS.get_enhancements(context.other_card)["m_bonus"] == true then
+                card.ability.extra.chips = (card.ability.extra.chips) + card.ability.extra.scale
+                return {
+                    message = "Upgrade!"
+                }
+            end
+        end
+        if context.cardarea == G.jokers and context.joker_main  then
+            return {
+                Xmult = card.ability.extra.mult,
+                x_chips = card.ability.extra.chips,
+            }
         end
         if context.forcetrigger then
             return {
-                mult = card.ability.extra.mult
-               }
+                Xmult = card.ability.extra.mult,
+                x_chips = card.ability.extra.chips,
+            }
         end
     end
 }
