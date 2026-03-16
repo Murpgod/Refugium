937637121d8745f519de70675142a2295512e4a8
GreenKookie56 <brandon05121@gmail.com>
2026-03-16 21:23:46 +0800

    Add files via upload

--- jokers/thepudding.lua
+++ jokers/thepudding.lua
@@ -1,19 +1,42 @@
 if Talisman then
 
+-- arrow format stolen from entropy
+function FormatArrowMult(arrows, mult)
+    mult = number_format(mult)
+    if to_big(arrows) < to_big(-1) then 
+        return "="..mult 
+    elseif to_big(arrows) < to_big(0) then 
+        return "+"..mult 
+    elseif to_big(arrows) < to_big(6) then 
+        if to_big(arrows) < to_big(1) then
+            return "X"..mult
+        end
+        local arr = ""
+        for i = 1, to_big(arrows):to_number() do
+            arr = arr.."^"
+        end
+        return arr..mult
+    else
+        return "{"..arrows.."}"..mult
+    end
+end
+
 SMODS.Joker{ --The Pudding
     key = "thepudding",
     config = {
         extra = {
-            mult = 1.17,
-            Spectral = 0
+            operator = 0,
+            mult = 17
         }
     },
     loc_txt = {
         ['name'] = 'The Pudding',
         ['text'] = {
-            [1] = 'Create a {C:dark_edition}Negative{} {C:spectral,E:1}Sholium{}',
-            [2] = 'if played hand contains',
-            [3] = 'a {C:clubs}Club{} and a {C:attention}7{}'
+            [1] = '{X:dark_edition,C:white}#1#{} Mult',
+            [2] = '{X:dark_edition,C:white}operator{} increases once if',
+            [3] = 'played hand contains a',
+            [4] = '{C:attention}Straight Flush{} and {C:attention}7{} of {C:clubs}Clubs{}',
+            [5] = ''
         },
         ['unlock'] = {
             [1] = ''
@@ -37,59 +60,73 @@ SMODS.Joker{ --The Pudding
     discovered = true,
     atlas = 'CustomJokers',
     in_pool = function(self, args)
-          return (
-          not args 
-          or args.source ~= 'buf' and args.source ~= 'jud' and args.source ~= 'rif' and args.source ~= 'rta' and args.source ~= 'sou' and args.source ~= 'uta' and args.source ~= 'wra' 
-          or args.source == 'sho'
-          )
-          and true
-      end,
-
+        return (
+            not args 
+            or args.source ~= 'buf' and args.source ~= 'jud' and args.source ~= 'rif' and args.source ~= 'rta' and args.source ~= 'sou' and args.source ~= 'uta' and args.source ~= 'wra' 
+            or args.source == 'sho'
+        )
+        and true
+    end,
+    
+    loc_vars = function(self, info_queue, card)
+        
+        return {vars = FormatArrowMult(card.ability.extra.operator, card.ability.extra.mult)}
+    end,
+    
     calculate = function(self, card, context)
-        if context.cardarea == G.jokers and context.joker_main  then
-            if ((function()
-    local suitCount = 0
-    for i, c in ipairs(context.full_hand) do
-        if c:is_suit("Clubs") then
-            suitCount = suitCount + 1
-        end
-    end
-    
-    return suitCount >= 1
-end)() and (function()
-    local rankCount = 0
-    for i, c in ipairs(context.full_hand) do
-        if c:get_id() == 7 then
-            rankCount = rankCount + 1
-        end
-    end
-    
-    return rankCount >= 1
-end)()) then
-                local created_consumable = true
-                G.E_MANAGER:add_event(Event({
+        if context.before and context.cardarea == G.jokers  and not context.blueprint then
+            if (next(context.poker_hands["Straight Flush"]) and (function()
+                local count = 0
+                for _, playing_card in pairs(context.full_hand or {}) do
+                    if playing_card:get_id() == 7 then
+                        count = count + 1
+                    end
+                end
+                return count >= 1
+            end)() and (function()
+                local count = 0
+                for _, playing_card in pairs(context.full_hand or {}) do
+                    if playing_card:is_suit("Clubs") then
+                        count = count + 1
+                    end
+                end
+                return count >= 1
+            end)()) then
+                return {
                     func = function()
-                        SMODS.add_card{set = 'Spectral', key = 'c_sholium_sholium', edition = 'e_negative', key_append = 'joker_forge_spectral'}
+                        card.ability.extra.operator = (card.ability.extra.operator) + 1
                         return true
                     end
-                }))
-                return {
-                    message = created_consumable and localize('k_plus_spectral') or nil
                 }
             end
         end
-        if context.forcetrigger then
-                local created_consumable = true
-                G.E_MANAGER:add_event(Event({
-                    func = function()
-                        SMODS.add_card{set = 'Spectral', key = 'c_sholium_sholium', edition = 'e_negative', key_append = 'joker_forge_spectral'}
-                        return true
-                    end
-                }))
-                return {
-                    message = created_consumable and localize('k_plus_spectral') or nil
-                }
+        if context.cardarea == G.jokers and context.joker_main or context.forcetrigger then
+			if to_big(card.ability.extra.operator) <= to_big(-1) then
+				return {
+					mult = lenient_bignum(card.ability.extra.mult)
+				}
+			elseif to_big(card.ability.extra.operator) == to_big(0) then
+				return {
+                    Xmult = lenient_bignum(card.ability.extra.mult)
+				}
+			elseif to_big(card.ability.extra.operator) == to_big(1) then
+				return {
+					emult = lenient_bignum(card.ability.extra.mult)
+				}
+			elseif to_big(card.ability.extra.operator) == to_big(2) then
+				return {
+					eemult = lenient_bignum(card.ability.extra.mult)
+				}
+			elseif to_big(card.ability.extra.operator) >= to_big(3) then
+				return {
+					hypermult = {
+						lenient_bignum(card.ability.extra.operator),
+						lenient_bignum(card.ability.extra.mult)
+					}
+				}
+			end
         end
     end
 }
+
 end
