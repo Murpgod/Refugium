3c3b68cfa2f16cc2a47d80cb5398673e8ef6e020
GreenKookie56 <brandon05121@gmail.com>
2026-03-16 20:52:01 +0800

    Update nanovirus.lua

--- jokers/nanovirus.lua
+++ jokers/nanovirus.lua
@@ -10,16 +10,15 @@ SMODS.Joker{ --Nano-virus
         ['name'] = 'Nano-virus',
         ['text'] = {
             [1] = 'Everything in shop',
-            [2] = 'costs {C:money}$1{} less',
-            [3] = ''
+            [2] = 'costs {C:money}$1{} less'
         },
         ['unlock'] = {
             [1] = 'Unlocked by default.'
         }
     },
     pos = {
-        x = 0,
-        y = 0
+        x = 1,
+        y = 3
     },
     display_size = {
         w = 71 * 1, 
@@ -27,12 +26,12 @@ SMODS.Joker{ --Nano-virus
     },
     cost = 6,
     rarity = 1,
-    blueprint_compat = true,
+    blueprint_compat = false,
     eternal_compat = true,
     perishable_compat = true,
     unlocked = true,
     discovered = true,
-    atlas = 'Joker',
+    atlas = 'CustomJokers',
     
     calculate = function(self, card, context)
     end,
@@ -64,7 +63,7 @@ local card_set_cost_ref = Card.set_cost
 function Card:set_cost()
     card_set_cost_ref(self)
     
-    if next(SMODS.find_card("j_modprefix_nanovirus")) then
+    if next(SMODS.find_card("j_sholextra_nanovirus")) then
         if (self.ability.set == 'Joker' or self.ability.set == 'Tarot' or self.ability.set == 'Planet' or self.ability.set == 'Spectral' or self.ability.set == 'Enhanced' or self.ability.set == 'Booster' or self.ability.set == 'Voucher') then
             self.cost = math.max(0, self.cost - (1))
         end
