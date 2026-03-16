774277823bd17a110fcc4ad0eff675a1a862db2f
GreenKookie56 <brandon05121@gmail.com>
2026-03-16 08:41:28 +0800

    whoops

--- jokers/glaivelord.lua
+++ jokers/glaivelord.lua
@@ -8,7 +8,7 @@ SMODS.Joker{ --Glaive Lord
         }
     },
     loc_txt = {
-        ['name'] = 'Glaive Lord (v48+)',
+        ['name'] = 'Glaive Lord',
         ['text'] = {
             [1] = 'This Joker gains {C:blue}+#2#{} Chips',
             [2] = 'for each {C:attention}non-first{} scored card',
