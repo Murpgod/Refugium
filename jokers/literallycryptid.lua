05082da48ca0a9c4b41d8de2079ea07fc0bd57ad
GreenKookie56 <brandon05121@gmail.com>
2026-03-18 20:55:07 +0800

    Add files via upload

--- jokers/literallycryptid.lua
+++ jokers/literallycryptid.lua
@@ -1,6 +1,11 @@
 
 SMODS.Joker{ --Literally Cryptid
     key = "literallycryptid",
+    config = {
+        extra = {
+            repetitions = 2
+        }
+    },
     loc_txt = {
         ['name'] = 'Literally Cryptid',
         ['text'] = {
@@ -13,8 +18,8 @@ SMODS.Joker{ --Literally Cryptid
         }
     },
     pos = {
-        x = 3,
-        y = 7
+        x = 0,
+        y = 0
     },
     display_size = {
         w = 71 * 1, 
@@ -32,29 +37,12 @@ SMODS.Joker{ --Literally Cryptid
     calculate = function(self, card, context)
         if context.cardarea == G.jokers and context.joker_main  and not context.blueprint then
             if to_big(#context.full_hand) == to_big(1) then
-                local cards_to_copy = {}
-                local target_index = 1
-                if context.full_hand[target_index] then
-                    table.insert(cards_to_copy, context.full_hand[target_index])
-                end
-                for i, source_card in ipairs(cards_to_copy) do
-                    G.playing_card = (G.playing_card and G.playing_card + 1) or 1
-                    local copied_card = copy_card(source_card, nil, nil, G.playing_card)
-                    copied_card:add_to_deck()
-                    G.deck.config.card_limit = G.deck.config.card_limit + 1
-                    table.insert(G.playing_cards, copied_card)
-                    G.hand:emplace(copied_card)
-                    copied_card.states.visible = nil
-                    G.E_MANAGER:add_event(Event({
-                        func = function() 
-                            copied_card:start_materialize()
-                            return true
-                        end
-                    }))
-                end
                 local target_joker = card
                 
                 if target_joker then
+                    if target_joker.ability.eternal then
+                        target_joker.ability.eternal = nil
+                    end
                     target_joker.getting_sliced = true
                     G.E_MANAGER:add_event(Event({
                         func = function()
@@ -62,11 +50,29 @@ SMODS.Joker{ --Literally Cryptid
                             return true
                         end
                     }))
-                    card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = "Destroyed!", colour = G.C.RED})
                 end
-                return {
-                    message = "Copied Card to Hand!"
-                }
+                for i = 1, 2 do
+                    local cards_to_copy = {}
+                    local target_index = 1
+                    if context.full_hand[target_index] then
+                        table.insert(cards_to_copy, context.full_hand[target_index])
+                    end
+                    for i, source_card in ipairs(cards_to_copy) do
+                        G.playing_card = (G.playing_card and G.playing_card + 1) or 1
+                        local copied_card = copy_card(source_card, nil, nil, G.playing_card)
+                        copied_card:add_to_deck()
+                        G.deck.config.card_limit = G.deck.config.card_limit + 1
+                        table.insert(G.playing_cards, copied_card)
+                        G.hand:emplace(copied_card)
+                        copied_card.states.visible = nil
+                        G.E_MANAGER:add_event(Event({
+                            func = function() 
+                                copied_card:start_materialize()
+                                return true
+                            end
+                        }))
+                    end
+                end
             end
         end
     end
