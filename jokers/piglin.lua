bb5f88a1f10c44985172225e3e32d64df0b358ec
GreenKookie56 <brandon05121@gmail.com>
2026-03-20 18:48:57 +0800

    jokerforge op

--- jokers/piglin.lua
+++ jokers/piglin.lua
@@ -3,7 +3,7 @@ SMODS.Joker{ --Piglin
     key = "piglin",
     config = {
         extra = {
-            tarot = 1
+            tarot = 0
         }
     },
     loc_txt = {
@@ -18,7 +18,7 @@ SMODS.Joker{ --Piglin
     },
     pos = {
         x = 0,
-        y = 7
+        y = 0
     },
     display_size = {
         w = 71 * 1, 
@@ -26,13 +26,13 @@ SMODS.Joker{ --Piglin
     },
     cost = 6,
     rarity = 1,
-    blueprint_compat = false,
-    demicoloncompat = true,
+    blueprint_compat = true,
     eternal_compat = true,
     perishable_compat = true,
     unlocked = true,
     discovered = true,
-    atlas = 'CustomJokers',
+    atlas = 'Joker',
+    pools = { ["modprefix_sholiumx_jokers"] = true },
     
     loc_vars = function(self, info_queue, card)
         
@@ -40,8 +40,8 @@ SMODS.Joker{ --Piglin
     end,
     
     calculate = function(self, card, context)
-        if context.individual and context.cardarea == G.play  then
-            if (SMODS.get_enhancements(context.other_card)["m_gold"] == true and to_big((card.ability.extra.tarot or 0)) == to_big(1)) then
+        if context.individual and context.cardarea == G.play  and not context.blueprint then
+            if (SMODS.get_enhancements(context.other_card)["m_gold"] == true and to_big((card.ability.extra.tarot or 0)) < to_big(1)) then
                 for i = 1, math.min(1, G.consumeables.config.card_limit - #G.consumeables.cards) do
                     G.E_MANAGER:add_event(Event({
                         trigger = 'after',
@@ -55,32 +55,30 @@ SMODS.Joker{ --Piglin
                     }))
                 end
                 delay(0.6)
-                card.ability.extra.tarot = 0
+                card.ability.extra.tarot = 1
                 return {
                     message = created_consumable and localize('k_plus_tarot') or nil
                 }
             end
         end
-        if context.after and context.cardarea == G.jokers  and not context.blueprint then
-            return {
-                func = function()
-                    card.ability.extra.tarot = 1
-                    return true
+        if context.cardarea == G.jokers and context.joker_main  then
+            if to_big(card.ability.extra.tarot) > to_big(0) then
+                for i = 1, math.min(1, G.consumeables.config.card_limit - #G.consumeables.cards) do
+                    G.E_MANAGER:add_event(Event({
+                        trigger = 'after',
+                        delay = 0.4,
+                        func = function()
+                            play_sound('timpani')
+                            SMODS.add_card({ set = 'Tarot', })                            
+                            card:juice_up(0.3, 0.5)
+                            return true
+                        end
+                    }))
                 end
-            }
-        end
-        if context.forcetrigger then
-            for i = 1, math.min(1, G.consumeables.config.card_limit - #G.consumeables.cards) do
-                G.E_MANAGER:add_event(Event({
-                    trigger = 'after',
-                    delay = 0.4,
-                    func = function()
-                        play_sound('timpani')
-                        SMODS.add_card({ set = 'Tarot', })                            
-                        card:juice_up(0.3, 0.5)
-                        return true
-                    end
-                }))
+                delay(0.6)
+                return {
+                    message = created_consumable and localize('k_plus_tarot') or nil
+                }
             end
         end
     end
